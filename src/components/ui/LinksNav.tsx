463d74a150057011d79b973920db482db0f68450
Teuku Rizal <cumilljr@gmail.com>
2025-04-11 22:35:42 +0700

    Update LinksNav.tsx

--- src/components/ui/LinksNav.tsx
+++ src/components/ui/LinksNav.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-import { NavItemHeaderAnimation } from '@/src/types';
+import { NavItemHeaderAnimation } from '@/types';
 
 export const navItemsSelected: { [key: string]: NavItemHeaderAnimation } = {
   '/': {
