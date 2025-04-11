349855f72ee56927899ef1023c848eec3792c1d5
Teuku Rizal <cumilljr@gmail.com>
2025-04-11 22:18:21 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,22 +3,22 @@
 import '@/src/styles/globals.css';
 import clsx from 'clsx';
 import local from 'next/font/local';
-import Header from '@/src/components/ui/Header';
-import Footer from '@/src/components/ui/Footer';
-import SplashCursor from '@/src/components/ui/SplashCursor';
+import Header from '@/components/ui/Header';
+import Footer from '@/components/ui/Footer';
+import SplashCursor from '@/components/ui/SplashCursor';
 import Head from './head';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import { HeroUIProvider } from '@heroui/react';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from '../components/utils/themeContext';
-import ProgressBar from '@/src/components/ui/progress';
-import BackToTopButton from '@/src/components/ui/BackToTopButton';
+import ProgressBar from '@/components/ui/progress';
+import BackToTopButton from '@/components/ui/BackToTopButton';
 import dynamic from 'next/dynamic';
 import React from 'react';
 
 // Dynamically import the Chatbot component with ssr: false (render only client-side)
-const Chatbot = dynamic(() => import('@/src/components/ui/ChatBot'), {
+const Chatbot = dynamic(() => import('@/components/ui/ChatBot'), {
   ssr: false
 });
 
