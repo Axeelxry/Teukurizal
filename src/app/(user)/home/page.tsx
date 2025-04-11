4a1b8eacd276cbc089956035b4d7d6fe4a404fbd
Teuku Rizal <cumilljr@gmail.com>
2025-04-11 22:31:58 +0700

    Update page.tsx

--- src/app/(user)/home/page.tsx
+++ src/app/(user)/home/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
-import { generateMetadata as getPageMetadata } from '@/src/components/utils/generateMetadata';
+import { generateMetadata as getPageMetadata } from '@/components/utils/generateMetadata';
 
 export async function generateMetadata() {
   return getPageMetadata({
